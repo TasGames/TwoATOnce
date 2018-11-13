2b808e83497f0a34dccb05ff37f707f6a938b02c
Thomas Simon <thomas.simon@btopenworld.com>
2018-11-13 11:10:21 +0000

    Created project

---
+++ GravityShift/Intermediate/Build/Win64/GravityShiftEditor/Development/Engine/SharedPCH.Engine.cpp
@@ -0,0 +1,1 @@
+#include "A:/Projects/Unreal Engine Projects/TwoATOnce/GravityShift/Intermediate/Build/Win64/GravityShiftEditor/Development/Engine/SharedPCH.Engine.h"
---
+++ GravityShift/Intermediate/Build/Win64/UE4Editor/Inc/GravityShift/GravityShiftGameModeBase.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef GRAVITYSHIFT_GravityShiftGameModeBase_generated_h
+#error "GravityShiftGameModeBase.generated.h already included, missing '#pragma once' in GravityShiftGameModeBase.h"
+#endif
+#define GRAVITYSHIFT_GravityShiftGameModeBase_generated_h
+
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_RPC_WRAPPERS
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAGravityShiftGameModeBase(); \
+	friend struct Z_Construct_UClass_AGravityShiftGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AGravityShiftGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/GravityShift"), NO_API) \
+	DECLARE_SERIALIZER(AGravityShiftGameModeBase)
+
+
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_INCLASS \
+private: \
+	static void StaticRegisterNativesAGravityShiftGameModeBase(); \
+	friend struct Z_Construct_UClass_AGravityShiftGameModeBase_Statics; \
+public: \
+	DECLARE_CLASS(AGravityShiftGameModeBase, AGameModeBase, COMPILED_IN_FLAGS(0 | CLASS_Transient), CASTCLASS_None, TEXT("/Script/GravityShift"), NO_API) \
+	DECLARE_SERIALIZER(AGravityShiftGameModeBase)
+
+
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AGravityShiftGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AGravityShiftGameModeBase) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AGravityShiftGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGravityShiftGameModeBase); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AGravityShiftGameModeBase(AGravityShiftGameModeBase&&); \
+	NO_API AGravityShiftGameModeBase(const AGravityShiftGameModeBase&); \
+public:
+
+
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AGravityShiftGameModeBase(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get()) : Super(ObjectInitializer) { }; \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AGravityShiftGameModeBase(AGravityShiftGameModeBase&&); \
+	NO_API AGravityShiftGameModeBase(const AGravityShiftGameModeBase&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AGravityShiftGameModeBase); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AGravityShiftGameModeBase); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AGravityShiftGameModeBase)
+
+
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_12_PROLOG
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_RPC_WRAPPERS \
+	GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_INCLASS \
+	GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_PRIVATE_PROPERTY_OFFSET \
+	GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_RPC_WRAPPERS_NO_PURE_DECLS \
+	GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_INCLASS_NO_PURE_DECLS \
+	GravityShift_Source_GravityShift_GravityShiftGameModeBase_h_15_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID GravityShift_Source_GravityShift_GravityShiftGameModeBase_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
